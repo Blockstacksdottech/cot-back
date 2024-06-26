0a3b38a13fb7c9c7496139bcd372c8908534cd4b
ahmedjb22 <a.jabnati@shellboxes.com>
2024-06-26 08:59:09 +0100

    updated data

--- datahandler/handler.py
+++ datahandler/handler.py
@@ -21,7 +21,6 @@ symbol_mapping = {
 
 relevant_contracts = ['USD INDEX - ICE FUTURES U.S.',
                       'EURO FX - CHICAGO MERCANTILE EXCHANGE',
-                      'GOLD - COMMODITY EXCHANGE INC.',
                       'BRITISH POUND - CHICAGO MERCANTILE EXCHANGE',
                       'JAPANESE YEN - CHICAGO MERCANTILE EXCHANGE',
                       'EURO FX/BRITISH POUND XRATE - CHICAGO MERCANTILE EXCHANGE',
@@ -30,7 +29,9 @@ relevant_contracts = ['USD INDEX - ICE FUTURES U.S.',
                       'NZ DOLLAR - CHICAGO MERCANTILE EXCHANGE',
                       'MEXICAN PESO - CHICAGO MERCANTILE EXCHANGE',
                       'AUSTRALIAN DOLLAR - CHICAGO MERCANTILE EXCHANGE',
-                      'NEW ZEALAND DOLLAR - CHICAGO MERCANTILE EXCHANGE'
+                      'WTI-PHYSICAL - NEW YORK MERCANTILE EXCHANGE',
+                      'GOLD - COMMODITY EXCHANGE INC.',
+                      'S&P 500 Consolidated - CHICAGO MERCANTILE EXCHANGE'
                       ]
 
 currency_to_symbol = {
@@ -39,13 +40,17 @@ currency_to_symbol = {
     'GOLD - COMMODITY EXCHANGE INC.': 'GOLD',
     'BRITISH POUND - CHICAGO MERCANTILE EXCHANGE': 'GBP',
     'JAPANESE YEN - CHICAGO MERCANTILE EXCHANGE': 'JPY',
-    'EURO FX/BRITISH POUND XRATE - CHICAGO MERCANTILE EXCHANGE': 'EURGBP',
+    'EURO FX/BRITISH POUND XRATE - CHICAGO MERCANTILE EXCHANGE': 'EUR',
     'CANADIAN DOLLAR - CHICAGO MERCANTILE EXCHANGE': 'CAD',
     'SWISS FRANC - CHICAGO MERCANTILE EXCHANGE': 'CHF',
     'NZ DOLLAR - CHICAGO MERCANTILE EXCHANGE': 'NZD',
     'MEXICAN PESO - CHICAGO MERCANTILE EXCHANGE': 'MXN',
     'AUSTRALIAN DOLLAR - CHICAGO MERCANTILE EXCHANGE': 'AUD',
-    'NEW ZEALAND DOLLAR - CHICAGO MERCANTILE EXCHANGE': 'NZD'
+    'NEW ZEALAND DOLLAR - CHICAGO MERCANTILE EXCHANGE': 'NZD',
+    'WTI-PHYSICAL - NEW YORK MERCANTILE EXCHANGE': "Crude OIL",
+    'GOLD - COMMODITY EXCHANGE INC.': "GOLD",
+    'S&P 500 Consolidated - CHICAGO MERCANTILE EXCHANGE': "S&P 500"
+
 }
 
 
@@ -95,13 +100,15 @@ def calculate_percentage_change(df):
     return df
 
 
-def calculate_5_week_avg_change(df):
-    df['5_week_change'] = df['pct_change'].rolling(window=5).mean()
+def calculate_5_week_avg_change(df, window):
+    df[f'{window}_week_change'] = df['pct_change'].rolling(
+        window=window).mean()
     return df
 
 
-def analyze_legacy_df(legacy_df, currency_pairs):
+def analyze_legacy_df(legacy_df, currency_pairs, isContract=False):
     combined_results = []
+    pair_result = {}
 
     for base_currency, quote_currency in currency_pairs:
         base_filtered_df = legacy_df[legacy_df['Market and Exchange Names'].str.contains(
@@ -128,22 +135,46 @@ def analyze_legacy_df(legacy_df, currency_pairs):
             'Net_Noncommercial_Positions_base': 'base_net_position',
             'Noncommercial Positions-Long (All)_quote': 'quote_long',
             'Noncommercial Positions-Short (All)_quote': 'quote_short',
-            'Net_Noncommercial_Positions_quote': 'quote_net_position'
+            'Net_Noncommercial_Positions_quote': 'quote_net_position',
+            'Commercial Positions-Long (All)_base': 'base_comm_long',
+            'Commercial Positions-Short (All)_base': 'base_comm_short',
+            'Net_Commercial_Positions_base': 'base_comm_net_position',
+            'Commercial Positions-Long (All)_quote': 'quote_comm_long',
+            'Commercial Positions-Short (All)_quote': 'quote_comm_short',
+            'Net_Commercial_Positions_quote': 'quote_comm_net_position',
+            'Nonreportable Positions-Long (All)_base': 'base_nonrep_long',
+            'Nonreportable Positions-Short (All)_base': 'base_nonrep_short',
+            'Net_Nonreportable_Positions_base': 'base_nonrep_net_position',
+            'Nonreportable Positions-Long (All)_quote': 'quote_nonrep_long',
+            'Nonreportable Positions-Short (All)_quote': 'quote_nonrep_short',
+            'Net_Nonreportable_Positions_quote': 'quote_nonrep_net_position',
         })
 
         # Add pair information
-        merged_df['pair'] = f'{currency_to_symbol[base_currency]}/{currency_to_symbol[quote_currency]}'
 
-        # Calculate pair positions based on the given rules
-        if quote_currency == "US Dollar":
-            merged_df['pair_long'] = merged_df['base_long']
-            merged_df['pair_short'] = merged_df['base_short']
-            merged_df['pair_net_position'] = merged_df['base_net_position']
-        elif base_currency == "US Dollar":
-            merged_df['pair_long'] = merged_df['quote_short']
-            merged_df['pair_short'] = merged_df['quote_long']
-            merged_df['pair_net_position'] = -merged_df['quote_net_position']
+        # print(f'Pair : {currency_to_symbol[base_currency]}/{currency_to_symbol[quote_currency]}')
+
+        if not isContract:
+            pair = f'{currency_to_symbol[base_currency]}/{currency_to_symbol[quote_currency]}'
+            merged_df['pair'] = pair
+            # Calculate pair positions based on the given rules
+            if quote_currency == "US Dollar":
+                merged_df['pair_long'] = merged_df['base_long']
+                merged_df['pair_short'] = merged_df['base_short']
+                merged_df['pair_net_position'] = merged_df['base_net_position']
+            elif base_currency == "US Dollar":
+                merged_df['pair_long'] = merged_df['quote_short']
+                merged_df['pair_short'] = merged_df['quote_long']
+                merged_df['pair_net_position'] = - \
+                    merged_df['quote_net_position']
+            else:
+                merged_df['pair_long'] = merged_df['base_long']
+                merged_df['pair_short'] = merged_df['base_short']
+                merged_df['pair_net_position'] = merged_df['base_net_position']
         else:
+            pair = f'{currency_to_symbol[base_currency]}'
+            print(f'Pair is {pair}')
+            merged_df['pair'] = pair
             merged_df['pair_long'] = merged_df['base_long']
             merged_df['pair_short'] = merged_df['base_short']
             merged_df['pair_net_position'] = merged_df['base_net_position']
@@ -152,7 +183,8 @@ def analyze_legacy_df(legacy_df, currency_pairs):
         merged_df = calculate_percentage_change(merged_df)
 
         # Calculate 5-week average percentage change
-        merged_df = calculate_5_week_avg_change(merged_df)
+        for i in range(2, 11):
+            merged_df = calculate_5_week_avg_change(merged_df, i)
 
         # Calculate sentiment
         merged_df['sentiment'] = merged_df.apply(
@@ -168,6 +200,11 @@ def analyze_legacy_df(legacy_df, currency_pairs):
 
         # Append to combined results
         combined_results.append(merged_df)
+        pair_result[pair] = merged_df
+    # print(f"combined length : {len(combined_results)}")
+    # for r in combined_results:
+        # print(r["pair"].head(1))
+    # return pair_result
 
     # Concatenate all results into a single DataFrame
     combined_df = pd.concat(combined_results, ignore_index=True)
@@ -190,8 +227,7 @@ def filter_and_analyze_legacy_data(final_data):
          "CANADIAN DOLLAR - CHICAGO MERCANTILE EXCHANGE"),
         ("AUSTRALIAN DOLLAR - CHICAGO MERCANTILE EXCHANGE",
          "USD INDEX - ICE FUTURES U.S."),
-        ("NEW ZEALAND DOLLAR - CHICAGO MERCANTILE EXCHANGE",
-         "USD INDEX - ICE FUTURES U.S.")
+        ("NZ DOLLAR - CHICAGO MERCANTILE EXCHANGE", "USD INDEX - ICE FUTURES U.S.")
     ]
 
     eur_pairs = [
@@ -206,7 +242,7 @@ def filter_and_analyze_legacy_data(final_data):
         ("EURO FX - CHICAGO MERCANTILE EXCHANGE",
          "AUSTRALIAN DOLLAR - CHICAGO MERCANTILE EXCHANGE"),
         ("EURO FX - CHICAGO MERCANTILE EXCHANGE",
-         "NEW ZEALAND DOLLAR - CHICAGO MERCANTILE EXCHANGE")
+         "NZ DOLLAR - CHICAGO MERCANTILE EXCHANGE")
     ]
 
     jpy_pairs = [
@@ -218,7 +254,7 @@ def filter_and_analyze_legacy_data(final_data):
          "JAPANESE YEN - CHICAGO MERCANTILE EXCHANGE"),
         ("AUSTRALIAN DOLLAR - CHICAGO MERCANTILE EXCHANGE",
          "JAPANESE YEN - CHICAGO MERCANTILE EXCHANGE"),
-        ("NEW ZEALAND DOLLAR - CHICAGO MERCANTILE EXCHANGE",
+        ("NZ DOLLAR - CHICAGO MERCANTILE EXCHANGE",
          "JAPANESE YEN - CHICAGO MERCANTILE EXCHANGE")
     ]
 
@@ -236,7 +272,7 @@ def filter_and_analyze_legacy_data(final_data):
         ("BRITISH POUND - CHICAGO MERCANTILE EXCHANGE",
          "AUSTRALIAN DOLLAR - CHICAGO MERCANTILE EXCHANGE"),
         ("BRITISH POUND - CHICAGO MERCANTILE EXCHANGE",
-         "NEW ZEALAND DOLLAR - CHICAGO MERCANTILE EXCHANGE")
+         "NZ DOLLAR - CHICAGO MERCANTILE EXCHANGE")
     ]
 
     chf_pairs = [
@@ -247,8 +283,13 @@ def filter_and_analyze_legacy_data(final_data):
          "SWISS FRANC - CHICAGO MERCANTILE EXCHANGE")
     ]
 
+    contracts = [(x, x) for x in relevant_contracts]
+
     # Combine all pairs
     all_currency_pairs = usd_pairs + eur_pairs + jpy_pairs + gbp_pairs + chf_pairs
+    all_currency_pairs = list(set(all_currency_pairs))
+    print(len(all_currency_pairs))
+    print(len(contracts))
 
     # Assuming final_data is loaded and passed to filter_legacy_df
     filtered_legacy_df = filter_legacy_df(final_data)
@@ -256,33 +297,53 @@ def filter_and_analyze_legacy_data(final_data):
     important_headers = [
         'date',
         'pair',
-        # Noncommercial Positions-Long (All) for the base currency
         'base_long',
-        # Noncommercial Positions-Short (All) for the base currency
         'base_short',
-        # Net Noncommercial Positions for the base currency
         'base_net_position',
-        # Noncommercial Positions-Long (All) for the quote currency
         'quote_long',
-        # Noncommercial Positions-Short (All) for the quote currency
         'quote_short',
-        # Net Noncommercial Positions for the quote currency
         'quote_net_position',
+        'base_comm_long',
+        'base_comm_short',
+        'base_comm_net_position',
+        'quote_comm_long',
+        'quote_comm_short',
+        'quote_comm_net_position',
+        'base_nonrep_long',
+        'base_nonrep_short',
+        'base_nonrep_net_position',
+        'quote_nonrep_long',
+        'quote_nonrep_short',
+        'quote_nonrep_net_position',
         # Noncommercial Positions-Long (All) for the pair
         'pair_long',
         # Noncommercial Positions-Short (All) for the pair
         'pair_short',
         'pair_net_position',
         'pct_change',
+        '2_week_change',
+        '3_week_change',
+        '4_week_change',
         '5_week_change',
+        '6_week_change',
+        '7_week_change',
+        '8_week_change',
+        '9_week_change',
+        '10_week_change',
         'sentiment'
     ]
 
     # Process all currency pairs
     legacy_df = filter_legacy_df(final_data)
-    analyzed_df = analyze_legacy_df(legacy_df, all_currency_pairs).fillna(0)
+   # Process all currency pairs
+    combined_df1 = analyze_legacy_df(
+        filtered_legacy_df, all_currency_pairs).fillna(0)
+    combined_df2 = analyze_legacy_df(
+        filtered_legacy_df, contracts, isContract=True).fillna(0)
+    combined_df = pd.concat([combined_df1, combined_df2], ignore_index=True)
+    res = combined_df.sort_values(by='date')[important_headers]
     # print(analyzed_df.head()[["Symbol","Decision",'Sentiment_Score']])
-    return analyzed_df
+    return res
 
 # Example call to the function
 # filter_and_analyze_tff_data(final_data)
@@ -364,11 +425,35 @@ def save_to_django_models(symbol_dataframes):
                 quote_long = data.get('quote_long', 0) or 0
                 quote_short = data.get('quote_short', 0) or 0
                 quote_net_position = data.get('quote_net_position', 0) or 0
+                base_comm_long = data.get('base_comm_long', 0) or 0
+                base_comm_short = data.get('base_comm_short', 0) or 0
+                base_comm_net_position = data.get(
+                    'base_comm_net_position', 0) or 0
+                quote_comm_long = data.get('quote_comm_long', 0) or 0
+                quote_comm_short = data.get('quote_comm_short', 0) or 0
+                quote_comm_net_position = data.get(
+                    'quote_comm_net_position', 0) or 0
+                base_nonrep_long = data.get('base_nonrep_long', 0) or 0
+                base_nonrep_short = data.get('base_nonrep_short', 0) or 0
+                base_nonrep_net_position = data.get(
+                    'base_nonrep_net_position', 0) or 0
+                quote_nonrep_long = data.get('quote_nonrep_long', 0) or 0
+                quote_nonrep_short = data.get('quote_nonrep_short', 0) or 0
+                quote_nonrep_net_position = data.get(
+                    'quote_nonrep_net_position', 0) or 0
                 pair_long = data.get('pair_long', 0) or 0
                 pair_short = data.get('pair_short', 0) or 0
                 pair_net_position = data.get('pair_net_position', 0) or 0
                 pct_change = data.get('pct_change', 0) or 0
+                two_week_change = data.get('2_week_change', 0) or 0
+                three_week_change = data.get('3_week_change', 0) or 0
+                four_week_change = data.get('4_week_change', 0) or 0
                 five_week_change = data.get('5_week_change', 0) or 0
+                six_week_change = data.get('6_week_change', 0) or 0
+                seven_week_change = data.get('7_week_change', 0) or 0
+                eight_week_change = data.get('8_week_change', 0) or 0
+                nine_week_change = data.get('9_week_change', 0) or 0
+                ten_week_change = data.get('10_week_change', 0) or 0
                 sentiment = data.get('sentiment', 'Neutral')
 
                 general_data = ProcessedData.objects.create(
@@ -380,11 +465,31 @@ def save_to_django_models(symbol_dataframes):
                     quote_long=quote_long,
                     quote_short=quote_short,
                     quote_net_position=quote_net_position,
+                    base_comm_long=base_comm_long,
+                    base_comm_short=base_comm_short,
+                    base_comm_net_position=base_comm_net_position,
+                    quote_comm_long=quote_comm_long,
+                    quote_comm_short=quote_comm_short,
+                    quote_comm_net_position=quote_comm_net_position,
+                    base_nonrep_long=base_nonrep_long,
+                    base_nonrep_short=base_nonrep_short,
+                    base_nonrep_net_position=base_nonrep_net_position,
+                    quote_nonrep_long=quote_nonrep_long,
+                    quote_nonrep_short=quote_nonrep_short,
+                    quote_nonrep_net_position=quote_nonrep_net_position,
                     pair_long=pair_long,
                     pair_short=pair_short,
                     pair_net_position=pair_net_position,
                     pct_change=pct_change,
+                    two_week_change=two_week_change,
+                    three_week_change=three_week_change,
+                    four_week_change=four_week_change,
                     five_week_change=five_week_change,
+                    six_week_change=six_week_change,
+                    seven_week_change=seven_week_change,
+                    eight_week_change=eight_week_change,
+                    nine_week_change=nine_week_change,
+                    ten_week_change=ten_week_change,
                     sentiment=sentiment
                 )
                 general_data.save()
@@ -416,25 +521,39 @@ def execute():
     important_headers = [
         'date',
         'pair',
-        # Noncommercial Positions-Long (All) for the base currency
         'base_long',
-        # Noncommercial Positions-Short (All) for the base currency
         'base_short',
-        # Net Noncommercial Positions for the base currency
         'base_net_position',
-        # Noncommercial Positions-Long (All) for the quote currency
         'quote_long',
-        # Noncommercial Positions-Short (All) for the quote currency
         'quote_short',
-        # Net Noncommercial Positions for the quote currency
         'quote_net_position',
+        'base_comm_long',
+        'base_comm_short',
+        'base_comm_net_position',
+        'quote_comm_long',
+        'quote_comm_short',
+        'quote_comm_net_position',
+        'base_nonrep_long',
+        'base_nonrep_short',
+        'base_nonrep_net_position',
+        'quote_nonrep_long',
+        'quote_nonrep_short',
+        'quote_nonrep_net_position',
         # Noncommercial Positions-Long (All) for the pair
         'pair_long',
         # Noncommercial Positions-Short (All) for the pair
         'pair_short',
         'pair_net_position',
         'pct_change',
+        '2_week_change',
+        '3_week_change',
+        '4_week_change',
         '5_week_change',
+        '6_week_change',
+        '7_week_change',
+        '8_week_change',
+        '9_week_change',
+        '10_week_change',
         'sentiment'
     ]
     important_data = analyzed_data[important_headers]
