12b6dd2f186a913efea1347def3ed0d69d3429e4
Vintex <vintex@Vintexs-MacBook-Pro.local>
2026-02-22 02:17:06 +0000

    fix updates

--- datahandler/scraper/Sentiment.py
+++ datahandler/scraper/Sentiment.py
@@ -7,6 +7,22 @@ from django.utils import timezone
 
 
 class Sentiment:
+    HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Referer": "https://www.myfxbook.com/",
+        "Origin": "https://www.myfxbook.com",
+        "Sec-Ch-Ua": '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": '"Windows"',
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1"
+    }
+
     def __init__(self):
         # Replace with the actual website URL
         self.url = 'https://www.myfxbook.com/community/outlook'
@@ -97,7 +113,7 @@ class Sentiment:
             list: A list of dictionaries containing symbol and outlook data (if found).
         """
 
-        response = requests.get(url, proxies=get_proxies())
+        response = requests.get(url, headers=self.HEADERS, proxies=get_proxies())
 
         soup = BeautifulSoup(response.content, 'html.parser')
 
